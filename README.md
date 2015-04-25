4088233e53b7badf48fabcb8617f56eeb85ccffa
Mayen Padua <edmarenpadua@gmail.com>
2015-04-25 10:51:18 +0800

    search in MongoDB

--- README.md
+++ README.md
@@ -2,9 +2,42 @@
 A web application using MySQL, MongoDB, and CouchDB
 
 
+########## MONGO DB STUFF ##########
 Set up for MongoDB:
 1. Make sure that you have MongoDB running as Windows Service
 2. Download the latest PHP Driver compatible for your machine
 3. Unzip the archive and put php_mongo.dll in your PHP extension directory ("ext" by default) and add the following line to your php.ini file: extension=php_mongo.dll
 4. Restart your XAMP/WAMPSERVER
+
+DB Set up:
+1. create a db named 'fruit'
+2. create a collection named 'fruit'
+3. insert a document of your liking to collection 'fruit'
+4. create a collection named 'fruit_price'
+3. insert a document of your liking to collection 'fruit'
+
+fruit.fruit.find() 			##to view all fruits in the fruit collection
+fruit.fruit_price.find() 	##to view all fruits in the fruit collection
+
+SCHEMA EXAMPLE:
+#collection: fruit
+{
+	_id : "553a599c3dddc0350584a994",
+	name: "mango",
+	quantity: "35",
+	distributor: "SM"
+}
+
+#collection: fruit_price
+{
+	_id : "443a678c3dddc0350584a545",
+	fruit_id: "553a599c3dddc0350584a994",
+	price: "350.00",
+	date: "04-25-2015"
+}
+
+#fruit_price collection is referenced to fruit
+collection using fruit_id
+
+####################################
 
--- mongodb/mongodb_connect.php
+++ mongodb/mongodb_connect.php
@@ -2,22 +2,38 @@
 
 // Config
 $dbhost = 'localhost';
-$dbname = 'zipcoll';
+$dbname = 'fruit';
  
 $db = new MongoClient('mongodb://localhost', array());
 
-$c1 = $db->selectCollection("test", "zipcoll");
+$c1 = $db->selectCollection($dbname, "fruit");
+$c2 = $db->selectCollection($dbname, "fruit_price");
 
-echo $c1;
 
+/*
+	SEARCH on fruit DB
+*/
 $cursor = $c1->find();
 
 foreach ($cursor as $doc) {
-    // do something to each document
-	$population = $doc['pop'];
-	$city = $doc['city'];
-	$state = $doc['state'];
-	echo 'City: '.$city.' State: '.$state.' Pop: '.$population."<br/>";
+    //for every fruit
+    $id = (string)$doc['_id'];
+	$name = $doc['name'];
+	$quantity = $doc['quantity'];
+	$distributor = $doc['distributor'];
+	echo 'Fruit Name: '.$id.'<br>Fruit Quantity: '.$quantity.'<br>Distributor: '.$distributor."<br/>";
+
+	//find the price changes using referencing bu fruit_id
+	$fruit_price_query = array('fruit_id' => $id);
+	$cursor2 = $c2->find($fruit_price_query);
+
+	foreach ($cursor2 as $doc2) {
+	    // do something to each document
+		$id = $doc2['fruit_id'];
+		$price = $doc2['price'];
+		$date = $doc2['date'];
+		echo '<br>Fruit ID: '.$id.'<br>Price: '.$price.'<br>Date: '.$date."<br/>";
+	}
 }
- echo "hello";
+
 ?>
