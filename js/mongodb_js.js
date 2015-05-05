34be5e559c28288933940459237cb925ecfa011d
Mayen Padua <edmarenpadua@gmail.com>
2015-05-05 15:40:08 +0800

    edit JS

--- js/mongodb_js.js
+++ js/mongodb_js.js
@@ -4,21 +4,13 @@ $(document).on("click", ".open-delete_fruit", function () {
      $("#delete_this").val(myId);
 });
 
-/*$(document).ready(function () {
-    $("#delete").click(function(){
-        console.log("hello");
-        id = $("#delete_this").val();
-         $.ajax({
-            type: "GET",
-            url: "test.php", //process to mail
-            data: ,
-            success: function () {
-                alert(id);
-            },
-            error: function(){
-                alert("failure");
-            }
-        });
-         console.log(id);
-    });
-});*/
+$(document).on("click", ".open-edit_fruit", function () {
+     var myId = $(this).data('id');
+     $("#edit_this").val(myId);
+
+     $("#name").val($(this).data('name'));
+     $("#quantity").val($(this).data('quantity'));
+     $("#distributor").val($(this).data('distributor'));
+     $("#price").val($(this).data('price'));
+});
+
